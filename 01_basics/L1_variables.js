dd3ea7fb4e0b3ca37cf3692faa9d31653982beee
Jaggu2029 <pawanjagushte@gmail.com>
2026-06-08 19:27:08 +0000

    lect 1

---
+++ 01_basics/L1_variables.js
@@ -0,0 +1,12 @@
+const AccountId= 12628;
+let AccountEmail ="pawanjaggu010@gmail.com"; // block scope
+var AccountPassword ="123030"; //function scope
+AccountCity="California";
+let AccountState ;
+
+ AccountEmail ="jaggu@gmail.com"; 
+ AccountPassword ="111111"; 
+AccountCity="washington";
+
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,AccountState])
+
