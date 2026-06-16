2c9c406217010efac07d3c459c6a6452709865e7
Jaggu2029 <pawanjagushte@gmail.com>
2026-06-16 06:50:49 +0000

    m

---
+++ 01_basics/L2_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat newer version of js
+//alert(3+3) // we are using node js and not browser
+
+let name = "pawan"
+let age =17
+let isLoggedIn = true
+
+// number => 2 to the pwr 53
+// bigint
+// boolean => true/false
+
+// null standalone value 
+//example  let temp ; => undefined
+//         let temp=null ---> khali nahi hai 
+//symbol => unique
+
+// object
+console.log(typeof age ); // number
+console.log(typeof "pawan" ); // string
+console.log(typeof null ); // object
+console.log(typeof undefined ); //undefined 
