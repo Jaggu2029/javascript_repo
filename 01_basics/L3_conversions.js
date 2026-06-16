d9ce29d9a280df7fcb5691ca1331e763e98ddb78
Jaggu2029 <pawanjagushte@gmail.com>
2026-06-16 08:29:52 +0000

    commiting

---
+++ 01_basics/L3_conversions.js
@@ -0,0 +1,22 @@
+let score = "33"; // 33
+console.log(score); 
+console.log(typeof (score));
+
+let value_in_number= Number(score); // coverts string into number data type
+console.log(value_in_number);
+console.log(typeof value_in_number)
+//node 01_basics/L3_conversions.js
+
+
+
+let score1 = "33abc" //try with nulll also and undefined
+console.log(score1); 
+console.log(typeof (score1));
+
+let value_in_number1= Number(score1); // coverts string into number data type
+console.log(value_in_number1);
+console.log(typeof value_in_number1)
+
+//if input above is true then val_in no is 1
+//if input above is "hoe" (string me kuch to hai) then convert it to bool we get 1 else 0 in ""(empty string)
+
