24a8a24f780df7f019440193cfef2fb0ef48e8d3
Jaggu2029 <pawanjagushte@gmail.com>
2026-06-16 08:41:57 +0000

    over

---
+++ 01_basics/L4_conversions.js
@@ -0,0 +1,12 @@
+let str1 ="Pawan"
+let str2 =" jagushte"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2 +2)  // number ko bhi bad me string hi deal karta hai 
+console.log(1+2 +"2") // left side me agar number dt hai then pehle woh add honge
+
+console.log(+true) // 1
+console.log(+"") // 0
